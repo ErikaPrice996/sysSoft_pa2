c4c2d883f1a6d12215b11f3e4310ec339f23353c
Erika Price <erika_price996@yahoo.com>
2025-11-21 10:26:20 -0500

    fixed insert to now insert into linked list where the hashed valued is in numerical order

--- insert.c
+++ insert.c
@@ -23,9 +23,23 @@ void insert(Command *cmd, int thread_id, uint32_t hashedName){
     strcpy(newRecord->name, cmd->name);
     newRecord->salary = cmd->salary;
 
-    // Insert at head of list 
-    newRecord->next = hash_table;
-    hash_table = newRecord;
-
+    // Insert hashRecord
+    if(hash_table == NULL) { //empty list
+        newRecord->next = NULL;
+        hash_table = newRecord;
+    } else if (hashedName < hash_table->hash) { //insert at head
+        newRecord->next = hash_table;
+        hash_table = newRecord;
+    } else { //insert in middle or end
+        hashRecord *prev = hash_table;
+        cur = hash_table->next;
+        while(cur != NULL && cur->hash < hashedName) {
+            prev = cur;
+            cur = cur->next;
+        }
+        newRecord->next = cur;
+        prev->next = newRecord;
+    }
+    
     printf("Inserted %u,%s,%u\n", hashedName, cmd->name, cmd->salary);
 }
